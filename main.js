4122db5ef9916d0b9aa6997ce8344f18a3556788
kadaro <97820280+rysry@users.noreply.github.com>
2022-11-04 15:46:44 +0800

    Add files via upload
    hehe

---
+++ main.js
@@ -0,0 +1,12 @@
+let text;
+let you = prompt("gala? [sge or ayoko] ")
+switch(you){
+    
+    case "sge":text="chat moko kung kelan hehe"
+       break;
+    case "ayoko":text="sgesgepo"
+       break;
+     default:text="ano?";
+}
+
+
